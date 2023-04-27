aebe32e0e6b6ea0744d0fbfd940a9ae4c99b4aeb
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-27 11:01:09 +0000

    initial commit

---
+++ ebenezer-weather-service/src/test/java/com/ebenezer/weather/WeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebenezer.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
