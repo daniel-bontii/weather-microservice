2e391d4550efd9284d8c582d398240252af8e7bc
KingKhay <ebenezerbuabeng60@gmail.com>
2023-04-28 11:33:37 +0000

    added api-gateway

--- samuel-weather-service/src/main/java/com/example/samuelWeatherService/Controller/WeatherController.java
+++ samuel-weather-service/src/main/java/com/example/samuelWeatherService/Controller/WeatherController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController()
-@RequestMapping("/api/v1/weather")
+@RequestMapping("/ninja-weather")
 @RequiredArgsConstructor
 public class WeatherController {
     private final WeatherServiceImpl weatherServiceImpl;
